6c660eef96c290c563d2804f13448a7a183bb62f
faheemxihawks <faheem@xihawks.com>
2025-11-13 12:55:20 +0500

    updated

--- src/components/HtmlEditor.tsx
+++ src/components/HtmlEditor.tsx
@@ -9,7 +9,7 @@ const HtmlEditor: React.FC<HtmlEditorProps> = ({ content, setContent }) => {
   return (
     <div className="bg-slate-900 rounded-b-xl">
       <textarea
-        className="w-full min-h-[26rem] p-6 bg-slate-900 text-slate-300 font-mono focus:outline-none rounded-b-xl resize-none"
+        className="w-full min-h-[26rem] text-green-300 text-xs p-6 bg-slate-900 text-slate-300 font-mono focus:outline-none rounded-b-xl resize-none"
         value={content}
         onChange={(e) => setContent(e.target.value)}
         placeholder="Edit the HTML directly..."
